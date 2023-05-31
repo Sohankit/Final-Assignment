0d1aa2527fab49e245ffe4e95135368587fff2f9
Sohankit <Sohankit@192.168.1.102>
2023-05-31 14:38:58 +0530

    commit

---
+++ Assignment9/src/in/ineuron/TestApp.java
@@ -0,0 +1,11 @@
+package in.ineuron;
+
+public class TestApp {
+	public static void main(String[] args) throws Exception {
+
+		Utility util = new Utility();
+		Producer producer = new Producer(util);
+		Consumer consumer = new Consumer(util);
+
+	}
+}
---
+++ Assignment9/src/in/ineuron/Utility.java
@@ -0,0 +1,40 @@
+package in.ineuron;
+import java.util.*;
+public class Utility {
+
+	private Queue<Integer> queue = new LinkedList<Integer>();
+	private final int queueCapacity = 5;
+	private boolean cond = false;
+
+	public synchronized void set(int i) throws Exception {
+
+		while (cond) {
+			wait();
+		}
+		queue.add(i);
+		cond = true;
+		notify();
+	}
+
+	public synchronized Integer get() throws Exception {
+
+		while (!cond) {
+			wait();
+		}
+		cond = false;
+		notify();
+		return queue.poll();
+	}
+
+	public Queue<Integer> getQueue() {
+		return queue;
+	}
+
+	public void setQueue(Queue<Integer> queue) {
+		this.queue = queue;
+	}
+
+	public int getQueueCapacity() {
+		return queueCapacity;
+	}
+}
